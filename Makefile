10ea96d9d10be0da51673b2a81ffea8926d8e74d
Gabor, Hoffer <gabor.hoffer@automic.com>
2017-08-09 09:28:58 +0200

    adding binary tree support, enhanced test

--- Makefile
+++ Makefile
@@ -1,21 +1,12 @@
 
-all: heap_tracer.so my-leaky-program heap_tracer2.so
+all: heap_tracer.so my-leaky-program
 clean:
 #	rm heap_tracer
 	rm -f heap_tracer.so
-	rm -f heap_tracer2.so
 	rm -f my-leaky-program
 
-#heap_tracer: heap_tracer.c
-#gcc -shared -fPIC memory_debug.c -o memory_debug.so
-#	gcc -rdynamic -Wno-deprecated-declarations -g -std=c99 heap_tracer.c -o heap_tracer
+heap_tracer.so: heap_tracer.c
+	gcc -shared -fPIC -rdynamic -Wno-deprecated-declarations -g -std=c99 heap_tracer.c -o heap_tracer.so
 
 my-leaky-program: my-leaky-program.c
 	gcc -rdynamic my-leaky-program.c -o my-leaky-program
-#	gcc my-leaky-program.c -o my-leaky-program
-
-heap_tracer.so: heap_tracer.c
-	gcc -rdynamic -Wno-deprecated-declarations -g3 -std=c99 -fPIC -shared heap_tracer.c -o heap_tracer.so
-
-heap_tracer2.so: heap_tracer2.c
-	gcc -rdynamic -Wno-deprecated-declarations -g3 -std=c99 -fPIC -shared heap_tracer2.c -o heap_tracer2.so
--- my-leaky-program.c
+++ my-leaky-program.c
@@ -5,49 +5,91 @@
 
 int counter = 0;
 
-int bar( void)
+#define ADDRESS_TABLE_SIZE	1024
+
+void	*addresses[ADDRESS_TABLE_SIZE];
+
+void bar0();
+void bar1();
+void bar2();
+void bar3();
+void foo();
+
+#define RECURSIVE_FUNCTION_BODY \
+	switch( rand() % 6) \
+	{ \
+		case 0: bar0(); break; \
+		case 1: bar1(); break; \
+		case 2: bar2(); break; \
+		case 4: bar3(); break; \
+		case 5: foo (); break; \
+		default: allocator(); break; \
+	}
+
+
+int allocator( void)
 {
-    long size;
-    char *p;
-
-    /*
-    size = 246;
-    p = malloc( size);
-    memset( p-4, 'H', size);
-    free( p);
-
-    p = malloc( size);
-    memset( p+4, 'H', size);
-    free( p);
-
-    return 0;
-    */ 
-    counter ++;
-    if ( counter < 5) bar();
-
-    for ( int i = 0; i < 1; i++)
+    long 	size;
+    int		i;
+    void	*p;
+/*
+    switch( rand() % 3)
     {
-	size = rand() % 1024;
+    case 0:
+    	for( i = 0; i < ADDRESS_TABLE_SIZE; i++)
+    		if ( addresses[i] == NULL)
+    			addresses[i] = malloc( rand() % 1024);
+    	break;
+    case 1:
+    {
+    	int	iteration = rand() % 1024, k;
+    	for( k = 0; k < iteration; k++)
+    	{
+
+    	}
+    }
+*/
+    size = rand() % 1024;
 	// printf( "allocate %ld bytes...\n", size);
 	p = malloc( size);
 	memset( p, 0x55, size);
-	p = realloc( p, size * 3);
-	memset( p, 0xaa, size * 3);
-	if ( i % 2) free( p);
-	// if ( i % 3) { free(p);free(p);free(p);free(p);}
-    }
-
-    // free( p);
+	if ( rand() % 2)
+	{
+		p = realloc( p, size * 3);
+		memset( p, 0xaa, size * 3);
+	}
+	if ( rand() % 2) free( p);
 
     return 0;
 }
 
-int foo()
+void bar0( void)
 {
-    bar();
+	RECURSIVE_FUNCTION_BODY
+}
+void bar1( void)
+{
+	RECURSIVE_FUNCTION_BODY
+}
+void bar2( void)
+{
+	RECURSIVE_FUNCTION_BODY
+}
+void bar3( void)
+{
+	RECURSIVE_FUNCTION_BODY
+}
+
+void foo()
+{
+	RECURSIVE_FUNCTION_BODY
 }
 
 int main( void)
 {
-    foo();
+	memset( addresses, 0, sizeof( addresses));
+    for( int i = 0; i < 10; i++)
+    {
+    	RECURSIVE_FUNCTION_BODY
+    }
 }
